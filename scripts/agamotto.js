7daaf4342d50101844ba7ca6dfbc6ea2c594c700
otacke <o.tacke@web.de>
2017-04-29 14:18:06 +0200

    move Polyfill to objects

--- scripts/agamotto.js
+++ scripts/agamotto.js
@@ -213,19 +213,5 @@ H5P.Agamotto = function ($) {
     return Math.min(hi, Math.max(lo, value));
   };
 
-  // Polyfill for "the one" browser that has hiccups ...
-  (function () {
-    if ( typeof window.CustomEvent === "function" ) return false;
-
-    function CustomEvent (event, params) {
-      params = params || {bubbles: false, cancelable: false, detail: undefined};
-      var evt = document.createEvent('CustomEvent');
-      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-      return evt;
-     }
-    CustomEvent.prototype = window.Event.prototype;
-    window.CustomEvent = CustomEvent;
-  })();
-
   return Agamotto;
 }(H5P.jQuery);
