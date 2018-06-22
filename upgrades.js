579318a5707431ef0a2e3864bfff1dd4da089a9c
Oliver Tacke <o.tacke@posteo.de>
2018-06-22 15:36:50 +0200

    Fix upgrade: Check for empty items first

--- upgrades.js
+++ upgrades.js
@@ -6,32 +6,34 @@ H5PUpgrades['H5P.Agamotto'] = (function ($) {
     1: {
       3: function (parameters, finished) {
         // Update image items
-        parameters.items = parameters.items.map( function (item) {
-          // Create new image structure
-          var newImage = {
-            library: 'H5P.Image 1.0',
-            // We avoid using H5P.createUUID since this is an upgrade script and H5P function may change
-            subContentId: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
-              var random = Math.random()*16|0, newChar = char === 'x' ? random : (random&0x3|0x8);
-              return newChar.toString(16);
-            }),
-            params: {
-              alt: item.labelText || '',
-              contentName: 'Image',
-              title: item.labelText || '',
-              file: item.image
-            }
-          };
+        if (parameters.items) {
+          parameters.items = parameters.items.map( function (item) {
+            // Create new image structure
+            var newImage = {
+              library: 'H5P.Image 1.0',
+              // We avoid using H5P.createUUID since this is an upgrade script and H5P function may change
+              subContentId: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
+                var random = Math.random()*16|0, newChar = char === 'x' ? random : (random&0x3|0x8);
+                return newChar.toString(16);
+              }),
+              params: {
+                alt: item.labelText || '',
+                contentName: 'Image',
+                title: item.labelText || '',
+                file: item.image
+              }
+            };
 
-          // Compose new item
-          item = {
-            description: item.description,
-            image: newImage,
-            labelText: item.labelText
-          };
+            // Compose new item
+            item = {
+              description: item.description,
+              image: newImage,
+              labelText: item.labelText
+            };
 
-          return item;
-        });
+            return item;
+          });
+        }
 
         finished(null, parameters);
       }
