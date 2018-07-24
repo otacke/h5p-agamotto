2d509be83523bd407efe55bbf6b0cbb3780f9273
Oliver Tacke <o.tacke@posteo.de>
2018-07-24 10:21:07 +0200

    HFP-1900 Update for metadata

--- upgrades.js
+++ upgrades.js
@@ -1,7 +1,7 @@
 /** @namespace H5PUpgrades */
 var H5PUpgrades = H5PUpgrades || {};
 
-H5PUpgrades['H5P.Agamotto'] = (function ($) {
+H5PUpgrades['H5P.Agamotto'] = (function () {
   return {
     1: {
       3: function (parameters, finished, extras) {
@@ -50,7 +50,24 @@ H5PUpgrades['H5P.Agamotto'] = (function ($) {
         delete parameters.title;
 
         finished(null, parameters, extras);
+      },
+      4: function (parameters, finished, extras) {
+        // Set new show title parameter
+        if (parameters.title) {
+          parameters.showTitle = true;
+        }
+
+        // Copy title to new metadata structure if present
+        var metadata = {
+          title: parameters.title || ((extras && extras.metadata) ? extras.metadata.title : undefined)
+        };
+        extras.metadata = metadata;
+
+        // Remove old parameter
+        delete parameters.title;
+
+        finished(null, parameters, extras);
       }
     }
   };
-})(H5P.jQuery);
+})();
